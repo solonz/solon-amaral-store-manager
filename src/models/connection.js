6d85d299e2ba42fd2ef25cefcc578f070a6cd906
Solon <amaral.solon@hotmail.com>
2022-10-18 15:20:23 -0300

    Correção de portas com ajuda de Bruno

--- src/models/connection.js
+++ src/models/connection.js
@@ -3,8 +3,8 @@ const mysql = require('mysql2/promise');
 const connection = mysql.createPool({
   host: process.env.MYSQL_HOST || 'localhost',
   port: process.env.MYSQL_PORT || 3306,
-  user: 'solon',
-  password: '19031958',
+  user: 'root',
+  password: 'password',
   database: 'StoreManager',
 });
 
